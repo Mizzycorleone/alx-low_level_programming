436fcdc99554f0e1b4a0fd99c36e40359f11493c
Mizzycorleone <Misanstevene@gmail.com>
2022-09-12 21:26:04 -0700

    7-9

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet reversely
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
